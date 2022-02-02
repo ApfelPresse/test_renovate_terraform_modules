c25d8e1a5f8bf0e67febe10749abf243a3cbb258
KevinStiefel <kevin.stiefel@deutschebahn.com>
2022-02-02 18:15:42 +0100

    wip /2

--- main.tf
+++ main.tf
@@ -1,3 +1,3 @@
 module "test" {
-  source = "git@github.com:ApfelPresse/test_renovate_terraform_modules.git/test_module"
+  source = "git@github.com:ApfelPresse/test_renovate_terraform_modules.git//test_module?ref=ae4b44ae6048456e123d85e6833d5ebcf6e61fde"
 }
